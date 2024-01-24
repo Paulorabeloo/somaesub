71e4c8511d16e982ad90eef621dacfebf6a84e65
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2024-01-24 20:08:51 -0300

    atvsomaesub

---
+++ atvsomaesub.c
@@ -0,0 +1,12 @@
+#include <stdio.h.>
+
+int main(){
+    int n1, n2, n3, soma, subtrair;
+    scanf("%i%i",&n1, &n2);
+
+    soma = n1 + n2;
+    scanf("%i",&n3);
+    subtrair = soma - n3;
+    printf("A resposta é %i", subtrair);
+return 0;
+}
